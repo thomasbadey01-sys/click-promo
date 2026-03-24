30e70920dfc4f7172fccdfb83f446bd3fed3c056
Thomas Badey <thombad12@gmail.com>
2026-03-24 16:32:42 +0000

    Update app pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,32 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import Dashboard from './pages/Dashboard';
+import PrivacyPolicy from './pages/PrivacyPolicy';
+import Abonnement from './pages/Abonnement';
 import Home from './pages/Home';
+import Carte from './pages/Carte';
+import OffreDetail from './pages/OffreDetail';
+import Favoris from './pages/Favoris';
 import Login from './pages/Login';
 import Profil from './pages/Profil';
-import OffreDetail from './pages/OffreDetail';
-import Admin from './pages/Admin';
-import PrivacyPolicy from './pages/PrivacyPolicy';
-import Dashboard from './pages/Dashboard';
-import Carte from './pages/Carte';
 import Feed from './pages/Feed';
-import Abonnement from './pages/Abonnement';
-import Favoris from './pages/Favoris';
+import Admin from './pages/Admin';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Navigate to="/Home" replace />} />
+        <Route path="/Dashboard" element={<Dashboard />} />
+        <Route path="/PrivacyPolicy" element={<PrivacyPolicy />} />
+        <Route path="/Abonnement" element={<Abonnement />} />
         <Route path="/Home" element={<Home />} />
+        <Route path="/Carte" element={<Carte />} />
+        <Route path="/OffreDetail" element={<OffreDetail />} />
+        <Route path="/Favoris" element={<Favoris />} />
         <Route path="/Login" element={<Login />} />
         <Route path="/Profil" element={<Profil />} />
-        <Route path="/OffreDetail" element={<OffreDetail />} />
-        <Route path="/Admin" element={<Admin />} />
-        <Route path="/PrivacyPolicy" element={<PrivacyPolicy />} />
-        <Route path="/Dashboard" element={<Dashboard />} />
-        <Route path="/Carte" element={<Carte />} />
         <Route path="/Feed" element={<Feed />} />
-        <Route path="/Abonnement" element={<Abonnement />} />
-        <Route path="/Favoris" element={<Favoris />} />
+        <Route path="/Admin" element={<Admin />} />
       </Routes>
     </BrowserRouter>
   )
