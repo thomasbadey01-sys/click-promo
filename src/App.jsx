86f34ff033b23d22f85a31e50d8fb454c0c661f1
Thomas Badey <thombad12@gmail.com>
2026-03-24 13:54:40 +0000

    Update app pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,30 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import Carte from './pages/Carte';
-import Favoris from './pages/Favoris';
+import OffreDetail from './pages/OffreDetail';
 import Home from './pages/Home';
 import PrivacyPolicy from './pages/PrivacyPolicy';
-import Profil from './pages/Profil';
-import Dashboard from './pages/Dashboard';
-import OffreDetail from './pages/OffreDetail';
-import Feed from './pages/Feed';
 import Abonnement from './pages/Abonnement';
 import Admin from './pages/Admin';
+import Feed from './pages/Feed';
+import Profil from './pages/Profil';
+import Dashboard from './pages/Dashboard';
+import Favoris from './pages/Favoris';
+import Carte from './pages/Carte';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Navigate to="/Home" replace />} />
-        <Route path="/Carte" element={<Carte />} />
-        <Route path="/Favoris" element={<Favoris />} />
+        <Route path="/OffreDetail" element={<OffreDetail />} />
         <Route path="/Home" element={<Home />} />
         <Route path="/PrivacyPolicy" element={<PrivacyPolicy />} />
-        <Route path="/Profil" element={<Profil />} />
-        <Route path="/Dashboard" element={<Dashboard />} />
-        <Route path="/OffreDetail" element={<OffreDetail />} />
-        <Route path="/Feed" element={<Feed />} />
         <Route path="/Abonnement" element={<Abonnement />} />
         <Route path="/Admin" element={<Admin />} />
+        <Route path="/Feed" element={<Feed />} />
+        <Route path="/Profil" element={<Profil />} />
+        <Route path="/Dashboard" element={<Dashboard />} />
+        <Route path="/Favoris" element={<Favoris />} />
+        <Route path="/Carte" element={<Carte />} />
       </Routes>
     </BrowserRouter>
   )
