1eb5f8b3c7fcd83d82e8955a5276e27f06c9a4da
Thomas Badey <thombad12@gmail.com>
2026-03-26 13:16:51 +0000

    File changes

--- src/pages/theme.jsx
+++ src/pages/theme.jsx
@@ -262,6 +262,15 @@ export const Ic = {
       <line x1="7" y1="7" x2="7.01" y2="7" />
     </svg>
   ),
+  share: (color, size) => (
+    <svg width={size} height={size} viewBox="0 0 24 24" fill="none" stroke={color} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+      <circle cx="18" cy="5" r="3" />
+      <circle cx="6" cy="12" r="3" />
+      <circle cx="18" cy="19" r="3" />
+      <line x1="8.59" y1="13.51" x2="15.42" y2="17.49" />
+      <line x1="15.41" y1="6.51" x2="8.59" y2="10.49" />
+    </svg>
+  ),
   plus: (color, size) => (
     <svg width={size} height={size} viewBox="0 0 24 24" fill="none" stroke={color} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
       <line x1="12" y1="5" x2="12" y2="19" />
