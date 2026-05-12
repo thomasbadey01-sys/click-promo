6be27f5628220d7288741f72b82c87fd395b980f
Thomas Badey <thombad12@gmail.com>
2026-05-12 09:34:16 +0000

    File changes

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,4 +1,5 @@
 import { base44 } from './base44Client';
 
-// Compatibility shim for legacy @/api/auth imports
 export const UserAuth = base44.auth;
+export const User = base44.auth;
+export default base44.auth;
